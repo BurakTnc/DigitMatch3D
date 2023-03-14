a4cd3b845b46f703746e3b44065b671e96e01221
BurakTnc <e.buraktnc@gmail.com>
2023-03-14 19:39:30 +0300

    m

--- Assets/_YabuGames/Scripts/Controllers/DigitController.cs
+++ Assets/_YabuGames/Scripts/Controllers/DigitController.cs
@@ -1,18 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class DigitController : MonoBehaviour
+namespace _YabuGames.Scripts.Controllers
 {
-    // Start is called before the first frame update
-    void Start()
+    public class DigitController : MonoBehaviour
     {
+        public int digitValue;
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
         
     }
 }
